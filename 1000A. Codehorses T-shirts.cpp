6cfff4c5326d509be45efbc4f39cf5b664fe9c85
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-11-11 02:32:19 -0800

    Create 1000A. Codehorses T-shirts.cpp

---
+++ 1000A. Codehorses T-shirts.cpp
@@ -0,0 +1,23 @@
+
+/*
+
+Codehorses has just hosted the second Codehorses Cup. 
+This year, the same as the previous one, organizers are giving T-shirts for the winners.
+
+The valid sizes of T-shirts are either "M" or from 0 to 3 "X" followed by "S" or "L". 
+For example, sizes "M", "XXS", "L", "XXXL" are valid and "XM", "Z", "XXXXL" are not.
+
+There are n winners to the cup for both the previous year and the current year. 
+Ksenia has a list with the T-shirt sizes printed for the last year cup and is yet to send the new list to the printing office.
+
+Organizers want to distribute the prizes as soon as possible, so now Ksenia is required not to write the whole list from the scratch 
+but just make some changes to the list of the previous year. 
+In one second she can choose arbitrary position in any word and replace its character with some uppercase Latin letter. 
+Ksenia can't remove or add letters in any of the words.
+
+What is the minimal number of seconds Ksenia is required to spend to change the last year list to the current one?
+
+The lists are unordered. 
+That means, two lists are considered equal if and only if the number of occurrences of any string is the same in both lists.
+
+*/
