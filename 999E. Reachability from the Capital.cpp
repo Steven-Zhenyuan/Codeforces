b1a4f6067385b1ac9e539d93d36d40460a7cbb13
Zhenyuan Xi <39312939+Zhenyuan-Xi@users.noreply.github.com>
2018-11-15 14:12:47 -0800

    Update 999E. Reachability from the Capital.cpp

--- 999E. Reachability from the Capital.cpp
+++ 999E. Reachability from the Capital.cpp
@@ -44,3 +44,89 @@ output
 1
 
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+const int MAXN=5050;
+
+struct node{
+    int to,next;
+}adj[MAXN];
+int head[MAXN],tot;
+int indegree[MAXN];
+
+int dfn[MAXN],low[MAXN],depth;
+int st[MAXN],top,vis[MAXN];
+int scc[MAXN],cnt;
+
+void init(){
+    tot=1;
+    depth=top=cnt=0;
+    memset(head,-1,sizeof(head));
+    memset(dfn,0,sizeof(dfn));
+    memset(low,0,sizeof(low));
+    memset(st,0,sizeof(st));
+    memset(vis,0,sizeof(vis));
+    memset(scc,0,sizeof(scc));
+    memset(indegree,0,sizeof(indegree));
+}
+
+void addEdge(int u,int v){
+    adj[tot].next=head[u];
+    adj[tot].to=v;
+    head[u]=tot++;
+}
+
+void tarjan(int u){
+    dfn[u]=low[u]=++depth;
+    st[++top]=u;
+    vis[u]=1;
+    for(int i=head[u];~i;i=adj[i].next){
+        int v=adj[i].to;
+        if(!dfn[v]){
+            tarjan(v);
+            low[u]=min(low[u],low[v]);
+        }else if(vis[v]) low[u]=min(low[u],dfn[v]);
+    }
+    if(dfn[u]==low[u]){
+        cnt++;
+        while(true){
+            int x=st[top--];
+            scc[x]=cnt;
+            vis[x]=0;
+            if(x==u) break;
+        }
+    }
+}
+
+int main(){
+    int n,m,s,u,v,i,j;
+    scanf("%d%d%d",&n,&m,&s);
+    init();
+    for(i=0;i<m;++i){
+        scanf("%d%d",&u,&v);
+        addEdge(u,v);
+    }
+    for(i=1;i<=n;++i){
+        if(!dfn[i]) tarjan(i);
+    }
+    indegree[scc[s]]=1;
+    for(i=1;i<=n;++i){
+        for(j=head[i];~j;j=adj[j].next){
+            if(scc[i]!=scc[adj[j].to]){
+                indegree[scc[adj[j].to]]++;
+            }
+        }
+    }
+    int ans=0;
+    for(i=1;i<=cnt;++i){
+        if(indegree[i]==0) ans++;
+    }
+    printf("%d",ans);
+}
+
+
+
+
